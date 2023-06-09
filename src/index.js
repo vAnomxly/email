9e03b77978e6049fab84e9cb0554b1625c186090
vAnomxly <elheat214@gmail.com>
2023-06-09 13:54:17 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 function App() {
-  const [email, setEmail] = useState('');
+  const [subscribed, setSubscribed] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Implement your email subscription logic here
-    console.log('Subscribed:', email);
-    alert('Thank you for subscribing!');
-    setEmail('');
+    const email = e.target.email.value;
+    // Perform any necessary logic with the email
+    console.log('Submitted email:', email);
+    // Reset the form
+    e.target.reset();
+    // Set subscribed state to true
+    setSubscribed(true);
   };
 
   return (
@@ -18,19 +22,15 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <input type="email" id="email" placeholder="Enter your email" required className="input-field" />
         </div>
         <button type="submit" className="button">Subscribe</button>
       </form>
+      {subscribed && (
+        <div className="alert">Subscribed!</div>
+      )}
     </div>
   );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
